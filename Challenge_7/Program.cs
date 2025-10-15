4b66e076622756525942edc1a246e2f2a0a2f961
Kingkonbro <konradboruch735@gmail.com>
2025-10-15 20:52:12 +0100

    Completed

--- Challenge_7/Program.cs
+++ Challenge_7/Program.cs
@@ -10,6 +10,38 @@ namespace Challenge_7
     {
         static void Main(string[] args)
         {
+            double prey = 30;
+            double predators = 5;
+            double a = 0.8;
+            double b = 0.5;
+            double c = 0.1;
+            double d = 0.3;
+            double e = 2.718;
+
+            double preyGrowthRate = 0;
+            double predatorsGrowthRate = 0;
+
+            int generation = 0;
+
+            while (((predators > 2) && (prey > 2)) && generation < 10 )
+            {
+                generation++;
+                Console.Write($"Generation: {generation}\t | ");
+
+                preyGrowthRate = Math.Pow(e,a-(c*predators));
+                prey *= preyGrowthRate;
+                prey = Math.Round(prey);
+                Console.Write($"Prey: {prey}\t |");
+
+                predatorsGrowthRate = Math.Pow(e, (d*c*prey)-b);
+                predators *= predatorsGrowthRate;
+                predators = Math.Round(predators);
+                Console.Write($"Predators: {predators}");
+
+                Console.WriteLine();
+            }
+            
+
         }
     }
 }
